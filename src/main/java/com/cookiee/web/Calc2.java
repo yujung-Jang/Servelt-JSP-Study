add62dabc08fcbc452179892ade3fac7b95c1cae
Yujung Jang <yjung.dev@gmail.com>
2022-01-04 18:19:22 +0900

    Cookie를 이용해 상태 값 유지하기

--- src/main/java/com/cookiee/web/Calc2.java
+++ src/main/java/com/cookiee/web/Calc2.java
@@ -4,17 +4,17 @@ import java.io.IOException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 @WebServlet("/calc2")
 public class Calc2 extends HttpServlet {
 	@Override
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
 	{
-		HttpSession session = request.getSession();
+		Cookie[] cookies = request.getCookies();
 		
 		response.setCharacterEncoding("UTF-8");
 		response.setContentType("text/html; charset=UTF-8");
@@ -27,9 +27,22 @@ public class Calc2 extends HttpServlet {
 		
 		// 계산
 		if(op.equals("=")) {
-			int x = (Integer)session.getAttribute("value"); // Object로 값 반환하므로 변환 필요
+			int x = 0;
+			for(Cookie c : cookies)
+				if(c.getName().equals("value")) {
+					x = Integer.parseInt(c.getValue());
+					break;
+				}
+			
 			int y = v; // 가장 최근에 입력받은 값
-			String operator = (String)session.getAttribute("op");
+			
+			String operator = "";
+			for(Cookie c : cookies)
+				if(c.getName().equals("op")) {
+					operator = c.getValue();
+					break;
+				}
+			
 			int result = 0;
 			
 			if(operator.equals("+"))
@@ -41,8 +54,10 @@ public class Calc2 extends HttpServlet {
 		}
 		// 값을 저장
 		else {
-			session.setAttribute("value", v);
-			session.setAttribute("op", op);
+			Cookie valueCookie = new Cookie("value", String.valueOf(v)); // 쿠키는 문자열만 가능
+			Cookie opCookie = new Cookie("op", op);
+			response.addCookie(valueCookie);
+			response.addCookie(opCookie);
 		}
 	}
 }
